c5e0cd3d1111182dc07494e4a99494cad0fbfb42
EpicAlbin03 <epicalbin03@gmail.com>
2022-01-12 22:13:11 +0100

    added docs

---
+++ docs/precache-manifest.facf3b90e5ce6caab49810d463d0b8de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437736ce191026dd79156d0307f4fcd4",
+    "url": "/simpletabmanager/index.html"
+  },
+  {
+    "revision": "aa337e1a42e2d2f61e83",
+    "url": "/simpletabmanager/static/js/2.8080d865.chunk.js"
+  },
+  {
+    "revision": "09ba896857c78b677e70b9b249be3b72",
+    "url": "/simpletabmanager/static/js/2.8080d865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebacda0bb4723ca2eece",
+    "url": "/simpletabmanager/static/js/main.1202723e.chunk.js"
+  },
+  {
+    "revision": "f5aa798d3d7e4f58595b",
+    "url": "/simpletabmanager/static/js/runtime-main.73f1db74.js"
+  }
+]);
